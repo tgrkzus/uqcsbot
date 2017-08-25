0c5df6445656d53d2f770b3dc4d2e196411e4db7
Tom Richardson <tgrkzus@gmail.com>
2017-08-25 19:19:42 +1000

    disabled plugins

--- config.py
+++ config.py
@@ -3,15 +3,18 @@ import os;
 
 BACKEND = 'Slack'
 
+# Bot config
 BOT_DATA_DIR = r'data/'
 BOT_EXTRA_PLUGIN_DIR = 'plugins/'
 
 BOT_LOG_FILE = r'errbot.log/'
 BOT_LOG_LEVEL = logging.DEBUG
 
-BOT_ADMINS = ('@tgrkzus', )
-
 BOT_PREFIX = '!'
 BOT_IDENTITY = {
         'token': os.environ["SLACK_BOT_TOKEN"] 
         }
+
+BOT_ADMINS = ('@tgrkzus', )
+
+CORE_PLUGINS = ()
