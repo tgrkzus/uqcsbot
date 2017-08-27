c900a529c0b2414aceb1bf165501e900e2bd3d06
Tom Richardson <tgrkzus@gmail.com>
2017-08-27 16:49:13 +1000

    added deps

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
+import os
 
+with open('requirements.txt') as f:
+        required = f.read().splitlines()
 setup(
         name='uqcsbot',
         version='1.0',
         description='Bot for uqcsbot',
         author='Various',
         packages=['uqcsbot'],
+        install_requres=required,
         )
