966e8d0a265ea61338de56928ac464bd3e416fd3
Tom Richardson <tgrkzus@gmail.com>
2017-08-27 16:52:21 +1000

    prob still not fixed

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ import os
 
 with open('requirements.txt') as f:
         required = f.read().splitlines()
+
 setup(
         name='uqcsbot',
         version='1.0',
         description='Bot for uqcsbot',
         author='Various',
         packages=['uqcsbot'],
-        install_requres=required,
+        install_requires=required,
         )
