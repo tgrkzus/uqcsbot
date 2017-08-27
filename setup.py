82c4201196a661e0d46ed7ab7791ecb6fcba5270
Tom Richardson <tgrkzus@gmail.com>
2017-08-27 20:47:53 +1000

    Fixed finally...

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ import os
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+# Install requirements backwards?
+required.reverse()
+
 setup(
         name='uqcsbot',
         version='1.0',
         description='Bot for uqcsbot',
         author='Various',
         packages=['uqcsbot'],
-        install_requires=required.reverse(), # It's backwards for some reason TODO fix
+        install_requires=required, # It's backwards for some reason TODO fix
         )
