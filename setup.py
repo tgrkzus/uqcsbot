3174dd7bcf51244fe7b326cbd89b69517a1c94c4
Tom Richardson <tgrkzus@gmail.com>
2017-08-28 14:54:22 +1000

    Added script to setuptools that prompts for tokens

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 from setuptools import setup
+from setuptools.command.install import install
 import os
+import readline
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-# Install requirements backwards?
-required.reverse()
+
+class SetupTokens(install):
+        def run(self):
+            install.run(self)
+            # Process tokens
+            botToken = ""
+            adminToken = ""
+
+            # Prompt for tokens or get current
+            print("")
+            print("Checking bot tokens...")
+            botToken = input("Slack bot token (Empty if unchanged): ")
+            os.environ["SLACK_BOT_TOKEN"] = botToken
+
+            adminToken = input("Slack admin token (Empty if unchanged:): ")
+            os.environ["SLACK_ADMIN_TOKEN"] = adminToken
+
+            print("")
+            print("Using tokens:")
+            print("SLACK_BOT_TOKEN: " + botToken)
+            print("SLACK_ADMIN_TOKEN: " + adminToken)
 
 setup(
         name='uqcsbot',
@@ -13,5 +34,7 @@ setup(
         description='Bot for uqcsbot',
         author='Various',
         packages=['uqcsbot'],
-        install_requires=required, # It's backwards for some reason TODO fix
+        install_requires=required,
+        cmdclass={'install': SetupTokens}
         )
+
