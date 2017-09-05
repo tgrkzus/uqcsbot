be662cd39c1c6b7c27b7fa482718ab5510e817b6
Tom Richardson <tgrkzus@gmail.com>
2017-09-05 10:33:59 +1000

    okay so my token script broken setuptools dependency installation

--- setup.py
+++ setup.py
@@ -6,28 +6,6 @@ import readline
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-
-class SetupTokens(install):
-        def run(self):
-            install.run(self)
-            # Process tokens
-            botToken = ""
-            adminToken = ""
-
-            # Prompt for tokens or get current
-            print("")
-            print("Checking bot tokens...")
-            botToken = input("Slack bot token (Empty if unchanged): ")
-            os.environ["SLACK_BOT_TOKEN"] = botToken
-
-            adminToken = input("Slack admin token (Empty if unchanged:): ")
-            os.environ["SLACK_ADMIN_TOKEN"] = adminToken
-
-            print("")
-            print("Using tokens:")
-            print("SLACK_BOT_TOKEN: " + botToken)
-            print("SLACK_ADMIN_TOKEN: " + adminToken)
-
 setup(
         name='uqcsbot',
         version='1.0',
@@ -35,6 +13,5 @@ setup(
         author='Various',
         packages=['uqcsbot'],
         install_requires=required,
-        cmdclass={'install': SetupTokens}
         )
 
